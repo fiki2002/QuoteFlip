e89adec6fba0ea655c4546987c369234bb28ec41
fiki2002 <adepitanoluwatosin202@gmail.com>
2023-09-18 11:49:53 +0100

    feat: added UI and model

---
+++ lib/model/quote_model.dart
@@ -0,0 +1,35 @@
+class QuoteModel {
+  final String id;
+  final String content;
+  final String author;
+  QuoteModel({
+    required this.id,
+    required this.content,
+    required this.author,
+  });
+
+  factory QuoteModel.fromJson(Map<String, dynamic> json) {
+    final id = json["id"];
+    if (id is! String) {
+      throw FormatException(
+          'Invalid JSON: required "id" field of type String in $json');
+    }
+
+    final content = json["content"];
+    if (content is! String) {
+      throw FormatException(
+          'Invalid JSON: required "content" field of type String in $json');
+    }
+
+    final author = json["author"];
+    if (author is! String) {
+      throw FormatException(
+          'Invalid JSON: required "author" field of type String in $json');
+    }
+    return QuoteModel(
+      id: id,
+      content: content,
+      author: author,
+    );
+  }
+}
